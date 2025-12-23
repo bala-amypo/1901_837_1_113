79b34588d7948e0a3fedb7eb15890b969c9accf0
bala-amypo <bala@amypo.in>
2025-12-23 17:11:11 +0000

    User commit

--- src/main/java/com/example/demo/service/PenaltyActionService.java
+++ src/main/java/com/example/demo/service/PenaltyActionService.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.PenaltyAction;
 import java.util.List;
 
 public interface PenaltyActionService {
-    List<PenaltyAction> getPenaltiesByCase(Long caseId);
-    PenaltyAction getPenaltyById(Long id);
     List<PenaltyAction> getAllPenalties();
+    PenaltyAction addPenalty(PenaltyAction penaltyAction);
 }
--- src/main/java/com/example/demo/service/impl/PenaltyActionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PenaltyActionServiceImpl.java
@@ -1,36 +1,27 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.IntegrityCase;
 import com.example.demo.entity.PenaltyAction;
-import com.example.demo.repository.IntegrityCaseRepository;
 import com.example.demo.repository.PenaltyActionRepository;
 import com.example.demo.service.PenaltyActionService;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
 public class PenaltyActionServiceImpl implements PenaltyActionService {
 
     private final PenaltyActionRepository penaltyActionRepository;
-    private final IntegrityCaseRepository integrityCaseRepository;
 
-    public PenaltyActionServiceImpl(
-            PenaltyActionRepository penaltyActionRepository,
-            IntegrityCaseRepository integrityCaseRepository) {
-
+    public PenaltyActionServiceImpl(PenaltyActionRepository penaltyActionRepository) {
         this.penaltyActionRepository = penaltyActionRepository;
-        this.integrityCaseRepository = integrityCaseRepository;
+    }
+
+    @Override
+    public List<PenaltyAction> getAllPenalties() {
+        return penaltyActionRepository.findAll();
     }
 
     @Override
     public PenaltyAction addPenalty(PenaltyAction penaltyAction) {
-
-        Long caseId = penaltyAction.getIntegrityCase().getId();
-
-        IntegrityCase integrityCase =
-                integrityCaseRepository.findById(caseId)
-                        .orElseThrow(() -> new IllegalArgumentException("case not found"));
-
-        integrityCase.setStatus("UNDER_REVIEW");
-        penaltyAction.setIntegrityCase(integrityCase);
-
         return penaltyActionRepository.save(penaltyAction);
     }
 }
