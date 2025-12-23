f3dcda31c4756a53d181fc7a2db0f07000e5a080
bala-amypo <bala@amypo.in>
2025-12-23 14:43:42 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,29 +1,51 @@
 package com.example.demo.dto;
 
 public class RegisterRequest {
+
     private String fullName;
     private String email;
     private String password;
-    private String roleName;
-    
-    public RegisterRequest() {}
-    
-    public RegisterRequest(String fullName, String email, String password, String roleName) {
+    private String role;
+
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String fullName, String email, String password, String role) {
         this.fullName = fullName;
         this.email = email;
         this.password = password;
-        this.roleName = roleName;
+        this.role = role;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
     
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
     
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
     
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
-    public String getRoleName() { return roleName; }
-    public void setRoleName(String roleName) { this.roleName = roleName; }
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
