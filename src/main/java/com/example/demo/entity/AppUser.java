6b63aec940016c9cdd69a060e9b7b5ac17b9afcc
bala-amypo <bala@amypo.in>
2025-12-23 16:33:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -23,22 +23,18 @@ public class AppUser {
     )
     private Set<Role> roles = new HashSet<>();
 
-    // No-arg constructor
     public AppUser() {}
-
-    // Constructor used in AuthServiceImpl
     public AppUser(String email, String password, String role) {
         this.email = email;
         this.password = password;
-        this.roles = new HashSet<>();
         this.roles.add(new Role(role));
     }
 
-    // Getters & Setters
     public Long getId() { return id; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
     public Set<Role> getRoles() { return roles; }
+
     public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) { this.password = password; }
     public void setRoles(Set<Role> roles) { this.roles = roles; }
