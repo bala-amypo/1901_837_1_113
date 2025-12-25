9dfdfaa816107dbdd6b67a00c7b2e6d9c1375fb2
bala-amypo <bala@amypo.in>
2025-12-25 03:48:03 +0000

    User commit

--- src/main/java/com/example/demo/servlet/BasicServlet.java
+++ src/main/java/com/example/demo/servlet/BasicServlet.java
@@ -4,19 +4,14 @@ import jakarta.servlet.http.*;
 import java.io.IOException;
 
 public class BasicServlet extends HttpServlet {
-
     @Override
-    protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp)
-            throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("Servlet is running");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req,
-                          HttpServletResponse resp)
-            throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setStatus(HttpServletResponse.SC_CREATED);
         resp.getWriter().write("Servlet POST handled");
     }
